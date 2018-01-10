c8a7a368698ddfb7b77ffbb3931ab61fcd493d38
@fbiville <fbiville@pivotal.io>
2018-01-10 17:56:15 +0100

    Mix and match Travis / Maven Central versions

--- src/main/kotlin/org/liquigraph/sentinel/SentinelRunner.kt
+++ src/main/kotlin/org/liquigraph/sentinel/SentinelRunner.kt
@@ -4,12 +4,14 @@ import org.liquigraph.sentinel.github.TravisYamlService
 import org.liquigraph.sentinel.mavencentral.MavenCentralService
 import org.liquigraph.sentinel.effects.Failure
 import org.liquigraph.sentinel.effects.Success
+import org.liquigraph.sentinel.github.LiquigraphService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 
 @Component
 class SentinelRunner(private val travisYamlService: TravisYamlService,
-                     private val mavenCentralServices: MavenCentralService): CommandLineRunner {
+                     private val mavenCentralServices: MavenCentralService,
+                     private val liquigraphService: LiquigraphService): CommandLineRunner {
 
     override fun run(vararg args: String?) {
         val testedNeo4jVersions = travisYamlService.getNeo4jVersions()
@@ -22,6 +24,9 @@ class SentinelRunner(private val travisYamlService: TravisYamlService,
                     is Success -> {
                         println("Fetched from Maven Central")
                         println(mavenCentralNeo4jVersions.content)
+                        println("Changing versions")
+                        println(liquigraphService.retainNewVersions(testedNeo4jVersions.content, mavenCentralNeo4jVersions.content))
+
                     }
                     is Failure -> {
                         System.err.println("Failed to fetch from Maven Central")
--- src/main/kotlin/org/liquigraph/sentinel/github/LiquigraphService.kt
+++ src/main/kotlin/org/liquigraph/sentinel/github/LiquigraphService.kt
@@ -1,5 +1,36 @@
 package org.liquigraph.sentinel.github
 
+import org.liquigraph.sentinel.mavencentral.MavenArtifact
+import org.springframework.stereotype.Service
+
+sealed class VersionChange
+data class Update(val old: SemanticVersion, val new: SemanticVersion) : VersionChange()
+data class Addition(val new: SemanticVersion) : VersionChange()
+
+@Service
 class LiquigraphService {
+    fun retainNewVersions(travisYmlVersions: List<TravisNeo4jVersion>, mavenArtifacts: List<MavenArtifact>): List<VersionChange> {
+        val travisVersions = travisYmlVersions.map { it.version }
+        val travisMajorVersions = travisVersions.map { it.major }
+        return mavenArtifacts.map { it.version }
+                .filter { travisMajorVersions.contains(it.major) }
+                .filter { it.isStable() }
+                .groupBy { Pair(it.major, it.minor) }
+                .mapNotNull { (majorMinor, mavenVersions) ->
+                    val mavenVersion = mavenVersions.max()!!
+                    val travisVersion = getLatestUpdateableVersion(travisVersions, majorMinor.first, majorMinor.second)
+                    when {
+                        travisVersion == null -> Addition(mavenVersion)
+                        travisVersion.compareTo(mavenVersion) != 0 -> Update(travisVersion, mavenVersion)
+                        else -> null
+                    }
+                }
+    }
+
+    private fun getLatestUpdateableVersion(travisVersions: List<SemanticVersion>, major: Int, minor: Int) =
+            travisVersions
+                    .sortedDescending()
+                    .dropLast(1) // we retain the lowest version to be confident we are compatible with, e.g., 3.0.0 -> 3.4.2
+                    .find { it.major == major && it.minor == minor }
 
 }
