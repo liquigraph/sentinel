9d50b14abf90093ac73eb7e6d2fb6449ea0ca979
Florent Biville <florent.biville@gmail.com>
2018-08-03 19:52:32 +0200

    Perform minor renames and move private classes

--- src/main/kotlin/org/liquigraph/sentinel/github/RawTravisYaml.kt
+++
@@ -1,8 +0,0 @@
-package org.liquigraph.sentinel.github
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.SOURCE)
-annotation class NoArgConstructor
-
-@NoArgConstructor data class RawTravisYaml(var env: RawTravisEnv)
-@NoArgConstructor data class RawTravisEnv(var matrix: List<String>)
